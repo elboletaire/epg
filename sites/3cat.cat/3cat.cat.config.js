e1d08af3d2dea9f953c9c268d5c485249635a262
Òscar Casajuana <elboletaire@gmail.com>
2025-02-27 22:53:29 +0100

    feat(3cat): add 3cat.cat parser

---
+++ sites/3cat.cat/3cat.cat.config.js
@@ -0,0 +1,51 @@
+const cheerio = require('cheerio')
+const dayjs = require('dayjs')
+const customParseFormat = require('dayjs/plugin/customParseFormat')
+dayjs.extend(customParseFormat)
+
+const channels = {
+  tv3: 'https://www.3cat.cat/Ccma/standalone/tv3_programacio_canal-tvc/graellacatchup/graella_tv3_{day}/0/0/',
+  324: 'https://www.3cat.cat/Comu/standalone/tv3_programacio_canal-324/contenidor/divgraella_324_{day}/0/0/',
+  sx3: 'https://www.3cat.cat/Comu/standalone/tv3_programacio_canal-sx3/contenidor/divgraella_cs3_{day}/0/0/',
+  c33: 'https://www.3cat.cat/Comu/standalone/tv3_programacio_canal-33/contenidor/divgraella_c33_{day}/0/0/',
+  esport3:
+    'https://www.3cat.cat/Comu/standalone/tv3_programacio_canal-esport3/contenidor/divgraella_es3_{day}/0/0/',
+  tv3cat:
+    'https://www.3cat.cat/Comu/standalone/tv3_programacio_canal-tv3cat/contenidor/divgraella_tvi_{day}/0/0/'
+}
+
+module.exports = {
+  site: '3cat.cat',
+  url({ channel, date }) {
+    const num = getDateNumber(date)
+    return channels[channel.site_id].replace('{day}', num)
+  },
+  parser({ content }) {
+    const $ = cheerio.load(content)
+    const programs = []
+    $('ul.programes > li').each((i, el) => {
+      programs.push({
+        title: $(el).find('.informacio-programa strong').text(),
+        start: dayjs($(el).attr('data-date'), 'DD/MM/YYYY HH:mm:ss'),
+        icon: $(el).find('img').attr('src')
+      })
+    })
+    return programs
+  },
+  channels() {
+    return Object.keys(channels).map(site_id => ({
+      site_id,
+      name: site_id.toUpperCase(),
+      lang: 'ca'
+    }))
+  }
+}
+
+function getDateNumber(date) {
+  const today = dayjs().startOf('day')
+  const targetDate = dayjs(date).startOf('day')
+
+  const diffDays = targetDate.diff(today, 'day')
+
+  return diffDays + 1
+}
---
+++ sites/3cat.cat/readme.md
@@ -0,0 +1,21 @@
+# 3cat.cat
+
+https://example.com
+
+### Download the guide
+
+```sh
+npm run grab --- --site=3cat.cat
+```
+
+### Update channel list
+
+```sh
+npm run channels:parse --- --config=./sites/3cat.cat/3cat.cat.config.js --output=./sites/3cat.cat/3cat.cat.channels.xml
+```
+
+### Test
+
+```sh
+npm test --- 3cat.cat
+```
